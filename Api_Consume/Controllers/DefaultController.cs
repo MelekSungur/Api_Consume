a6fc13b5699a16ac111c19c9bce71c5ffec7ca64
melek <melek@MELEK>
2023-01-24 21:11:04 +0300

    Add project files.

---
+++ Api_Consume/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace Api_Consume.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
